547ef7838e5707e1b1eb12be71ea5c8c4ff1e264
Vikram madankar <33344492+vrock11@users.noreply.github.com>
2021-07-30 12:43:51 +0530

    Added some files

---
+++ App.js
@@ -0,0 +1,116 @@
+// Challenge 1 : Your Age in Days
+
+function ageInDays() {
+  var birthYear = prompt("what year were you born... Good Friend?");
+  var ageInDays = (2021 - birthYear) * 365;
+  var h1 = document.createElement("h1");
+  var textAnswer = document.createTextNode("You are" + ageInDays + "days old");
+  h1.setAttribute("id", "ageInDays");
+  h1.setAttribute("style", "color:yellow");
+  h1.appendChild(textAnswer);
+  document.getElementById("flex-box-result").appendChild(h1);
+}
+
+function reset() {
+  document.getElementById("ageInDays").remove();
+}
+
+// Challenge 2 : Cat Generator
+function generateCat() {
+  var image = document.createElement("img");
+  var div = document.getElementById("flex-cat-gen");
+  image.src = " http://thecatapi.com/api/images/get?format=src&type=gif ";
+  div.appendChild(image);
+}
+
+//Challenge 3: rps game
+function rpsGame(yourChoice) {
+  console.log(yourChoice);
+
+  var humanChoice, botChoice;
+  humanChoice = yourChoice.id;
+  botChoice = numberToChoice(randToRpsInt());
+  console.log("Computer choice:", botChoice);
+
+  results = decideWinner(humanChoice, botChoice);
+  console.log(results);
+
+  message = finalMessage(results);
+  console.log(message);
+
+  rpsFrontEnd(yourChoice.id, botChoice, message);
+
+  //Random number generation to the Rps game...
+  function randToRpsInt() {
+    return Math.floor(Math.random() * 3);
+  }
+  function numberToChoice(number) {
+    return ["rock", "paper", "scissors"][number];
+  }
+
+  // Decide winner
+  function decideWinner(yourChoice, computerChoice) {
+    var rpsDatabase = {
+      rock: { scissors: 1, rock: 0.5, paper: 0 },
+      paper: { rock: 1, paper: 0.5, scissors: 0 },
+      scissors: { paper: 1, paper: 0.5, rock: 0 },
+    };
+
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+    return [yourScore, computerScore];
+  }
+
+  // final message
+  function finalMessage([yourScore, computerScore]) {
+    if (yourScore === 0) {
+      return { message: "You lost!", color: "red" };
+    } else if (yourScore === 0.5) {
+      return { message: "You tied!", color: "yellow" };
+    } else {
+      return { message: "You won!", color: "green" };
+    }
+  }
+
+  //Uses for displaying FrontEnd part
+  function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
+    var ImageDatabase = {
+      rock: document.getElementById("rock").src,
+      paper: document.getElementById("paper").src,
+      scissors: document.getElementById("scissors").src,
+    };
+
+    //let's remove all the images
+    document.getElementById("rock").remove();
+    document.getElementById("paper").remove();
+    document.getElementById("scissors").remove();
+
+    var humanDiv = document.createElement("div");
+    var botDiv = document.createElement("div");
+    var messageDiv = document.createElement("div");
+
+    humanDiv.innerHTML =
+      "<img src='" +
+      ImageDatabase[humanImageChoice] +
+      "'height=150 width=150 style='box-shadow:0px 10px 50px rgba(37,50,233,1);'>";
+    messageDiv.innerHTML =
+      "<h1 style='color: " +
+      finalMessage["color"] +
+      "font-size:60px; padding:30px; '>" +
+      finalMessage["message"] +
+      "</h1>";
+    botDiv.innerHTML =
+      "<img src='" +
+      ImageDatabase[botImageChoice] +
+      "'height=150 width=150 style='box-shadow:0px 10px 50px rgba(243,38,24,1);'>";
+
+    document.getElementById("flex-box-rps-div").appendChild(humanDiv);
+    document.getElementById("flex-box-rps-div").appendChild(messageDiv);
+    document.getElementById("flex-box-rps-div").appendChild(botDiv);
+  }
+}
+
+//Challenge:4 Change the color of all buttons
+
+var all_buttons 
