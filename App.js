d54afb8bef0422c5ed3d088f451a5b993b83f1e5
vrock11 <madankarvikram@gmail.com>
2021-07-30 14:56:45 +0530

    [ADDED] JS to btn clr game

--- App.js
+++ App.js
@@ -113,5 +113,55 @@ function rpsGame(yourChoice) {
 
 //Challenge:4 Change the color of all buttons
 
-var all_buttons = document.getElementsByTagName('button');
-console.log(all_buttons);
+var all_buttons = document.getElementsByTagName("button");
+var copyAllButtons = [];
+for (let i = 0; i < all_buttons.length; i++) {
+  copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+// btn -color-changer
+function buttonColorChange(buttonThingy) {
+  if (buttonThingy.value === "red") {
+    buttonRed();
+  } else if (buttonThingy.value === "green") {
+    buttonGreen();
+  } else if (buttonThingy.value === "reset") {
+    buttonColorReset();
+  } else if (buttonThingy.value === "random") {
+    randomColors();
+  }
+}
+
+//RedButton function
+function buttonRed() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add("btn-danger");
+  }
+}
+
+// Green btn
+function buttonGreen() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add("btn-success");
+  }
+}
+
+//button color reset
+function buttonColorReset() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add(copyAllButtons[i]);
+  }
+}
+
+//Random-btn colors
+function randomColors() {
+  let choices = ["btn-primary", "btn-danger", "btn-success", "btn-warning"];
+  for (let i = 0; i < all_buttons.length; i++) {
+    let randomNumber = Math.floor(Math.random() * 4);
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add(choices[randomNumber]);
+  }
+}
